6f153ffbd6eceea77eac8df301a103980c695027
Apurva <66008516+apurvagokhale29@users.noreply.github.com>
2023-06-05 12:50:56 +0530

    Create Question3.js

---
+++ Question3.js
@@ -0,0 +1,16 @@
+// Q3. Write a program to check if two strings are a rotation of each other?
+
+const str1 = 'hello';
+const str2 = 'llohe';
+
+function areStringsRotations(str1, str2) {
+  // Check if the two strings are the same length and non-empty
+  if (str1.length !== str2.length || str1.length === 0) {
+    return false;
+  }
+  // Concatenate the first string with itself and check if the second string is a substring of the result
+  const concatStr = str1 + str1;
+  return concatStr.includes(str2);
+}
+console.log(areStringsRotations(str1, str2)); // Output: true
+
